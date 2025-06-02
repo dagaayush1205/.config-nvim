152b030511a2e13f1b15b5abbc29b8d572cb69e7
ayush <ad6027@srmist.edu.in>
2025-06-02 12:07:34 +0530

    included 2 new themees

---
+++ lua/plugins/nightfly.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false, -- Set to false to load it on startup, or true if you want to lazy-load it (see notes below)
+    priority = 1000, -- Important for colorschemes to load early
+  },
+}
